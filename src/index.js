44819603e998619ea859e9c43492c829c4389bdd
rafrex <code@rafrex.com>
2016-07-07 20:25:03 -0700

    Replace this with detectHover to prevent scope issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const detectHover = {
   update() {
     if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
-      this.hover = window.matchMedia('(hover: hover)').matches;
-      this.none = window.matchMedia('(hover: none)').matches;
-      this.onDemand = window.matchMedia('(hover: on-demand)').matches;
-      this.anyHover = window.matchMedia('(any-hover: hover)').matches;
-      this.anyNone = window.matchMedia('(any-hover: none)').matches;
-      this.anyOnDemand = window.matchMedia('(any-hover: on-demand)').matches;
+      detectHover.hover = window.matchMedia('(hover: hover)').matches;
+      detectHover.none = window.matchMedia('(hover: none)').matches;
+      detectHover.onDemand = window.matchMedia('(hover: on-demand)').matches;
+      detectHover.anyHover = window.matchMedia('(any-hover: hover)').matches;
+      detectHover.anyNone = window.matchMedia('(any-hover: none)').matches;
+      detectHover.anyOnDemand = window.matchMedia('(any-hover: on-demand)').matches;
     }
   },
 };
