30b82520c7b35fb520878098a5cd737868f6577b
rafrex <code@rafrex.com>
2016-07-11 11:41:25 -0700

    Remove hover onDemand state - on-demand now matches to none
    The July 6th 2016 W3C Media Queries Level 4 specification
    removes on-demand as one of the values. If a device registers
    that it has the hover on-demand capability, detectHover will
    set its own state to hover none. The same applies to any hover.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const detectHover = {
   update() {
     if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
       detectHover.hover = window.matchMedia('(hover: hover)').matches;
-      detectHover.none = window.matchMedia('(hover: none)').matches;
-      detectHover.onDemand = window.matchMedia('(hover: on-demand)').matches;
+      detectHover.none = (
+        window.matchMedia('(hover: none)').matches ||
+        window.matchMedia('(hover: on-demand)').matches
+      );
       detectHover.anyHover = window.matchMedia('(any-hover: hover)').matches;
-      detectHover.anyNone = window.matchMedia('(any-hover: none)').matches;
-      detectHover.anyOnDemand = window.matchMedia('(any-hover: on-demand)').matches;
+      detectHover.anyNone = (
+        window.matchMedia('(any-hover: none)').matches ||
+        window.matchMedia('(any-hover: on-demand)').matches
+      );
     }
   },
 };
