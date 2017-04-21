4d9f9b4d9fe59f687a48133e1ffa96e86e226abf
Thomas <thomas.tilkema@tnt-digital.com>
2017-04-21 11:40:08 +0200

    Added Typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for detectHover
+// Project: https://github.com/rafrex/detect-hover
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+declare namespace detectHover
+{
+  const anyHover: boolean;
+  const anyNone: boolean;
+  const hover: boolean;
+  const none: boolean;
+
+  const update: () => void;
+}
+
+export default detectHover;
